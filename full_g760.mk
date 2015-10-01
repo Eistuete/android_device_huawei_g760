66b4f41087711fdcf9bc19838394545df038f9cb
Eistuete <vendor.hwg7@gmail.com>
2015-10-01 08:06:47 +0200

    Update full_g760.mk

--- full_g760.mk
+++ full_g760.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/huawei/g760/g760.mk)
-$(call inherit-product-if-exists, vendor/huawei/g760/g760-vendor.mk)
+
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := g760
