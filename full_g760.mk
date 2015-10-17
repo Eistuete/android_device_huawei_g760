545e50e541907f87fa4b754e81b5ea78718d1895
Eistuete <vendor.hwg7@gmail.com>
2015-10-17 06:44:32 +0200

    Update full_g760.mk

--- full_g760.mk
+++ full_g760.mk
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
